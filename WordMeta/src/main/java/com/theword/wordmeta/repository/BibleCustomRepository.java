8acdc623371b53a745e59987ad3f6b8a423247f1
tobintom <tobintom@gmail.com>
2020-05-15 13:03:40 -0500

    new changes for cross reference

--- WordMeta/src/main/java/com/theword/wordmeta/repository/BibleCustomRepository.java
+++ WordMeta/src/main/java/com/theword/wordmeta/repository/BibleCustomRepository.java
@@ -1,11 +1,15 @@
 package com.theword.wordmeta.repository;
 
+import java.util.List;
+
 import com.theword.wordmeta.model.Bible;
 import com.theword.wordmeta.model.Book;
+import com.theword.wordmeta.model.CrossReference;
 
 public interface BibleCustomRepository {
 
 	Bible getBibleBooks(String id);
 	Book getBookChapters(String bibleId, String bookCode);
 	Book getChapterVerses(String bibleId, String bookCode,String chapter);
+	List<CrossReference> getRawCrossReference(String book, String chapter, String verse);
 }
